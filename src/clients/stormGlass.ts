90ce0da57ff108b5e1cdfc6995351e140c9648e8
Fridry <fredwin.ed@gmail.com>
2021-05-20 21:09:26 -0300

    Criação de funções e interfaces para normalizar a resposta da API StormGlass

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,4 +1,35 @@
-import { AxiosStatic } from "axios";
+import { ForecastController } from '@src/controllers/forecast';
+import { AxiosStatic } from 'axios';
+
+export interface StormGlassPointSource {
+  [key: string]: number
+}
+
+export interface StormGlassPoint {
+  readonly time: string
+  readonly swellDirection: StormGlassPointSource
+  readonly swellHeight: StormGlassPointSource
+  readonly swellPeriod: StormGlassPointSource
+  readonly waveDirection: StormGlassPointSource
+  readonly waveHeight: StormGlassPointSource
+  readonly windDirection: StormGlassPointSource
+  readonly windSpeed: StormGlassPointSource
+}
+
+export interface StormGlassForecastResponse {
+  hours: StormGlassPoint[]
+}
+
+export interface ForecastPoint {
+  time: string
+  swellDirection: number
+  swellHeight: number
+  swellPeriod: number
+  waveDirection: number
+  waveHeight:number
+  windDirection: number
+  windSpeed: number
+}
 
 export class StormGlass {
   readonly stormGlassAPIParams = 'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed'
@@ -8,8 +39,34 @@ export class StormGlass {
 
   }
 
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  public async fetchPoints(lat: number, lng: number): Promise<{}> {
-    return this.request.get(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1621545703`)
+  public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
+    const response = await this.request.get<StormGlassForecastResponse>(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1621545703`)
+    return this.normalizeResponse(response.data)
+  }
+
+  private normalizeResponse(points: StormGlassForecastResponse): ForecastPoint[] {
+    return points.hours.filter(this.isValidPoint.bind(this)).map(point => ({
+      time: point.time,
+      swellDirection: point.swellDirection[this.stormGlassAPISource],
+      swellHeight: point.swellHeight[this.stormGlassAPISource],
+      swellPeriod: point.swellPeriod[this.stormGlassAPISource],
+      waveDirection: point.waveDirection[this.stormGlassAPISource],
+      waveHeight:point.waveHeight[this.stormGlassAPISource],
+      windDirection: point.windDirection[this.stormGlassAPISource],
+      windSpeed: point.windSpeed[this.stormGlassAPISource]
+    }))
+  }
+
+  private isValidPoint(point: Partial<StormGlassPoint>): boolean {
+    return !!(
+      point.time &&
+      point.swellDirection?.[this.stormGlassAPISource] &&
+      point.swellHeight?.[this.stormGlassAPISource] &&
+      point.swellPeriod?.[this.stormGlassAPISource] &&
+      point.waveDirection?.[this.stormGlassAPISource] &&
+      point.waveHeight?.[this.stormGlassAPISource] &&
+      point.windDirection?.[this.stormGlassAPISource] &&
+      point.windSpeed?.[this.stormGlassAPISource]
+    )
   }
 }
